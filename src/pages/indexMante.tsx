da30c8f446028e66bde5a4a53c1053b8265d6363
JulioJafed <128091133+JulioJafed@users.noreply.github.com>
2023-06-08 13:59:47 -0600

    new css style indexMante

--- src/pages/indexMante.tsx
+++ src/pages/indexMante.tsx
@@ -2,8 +2,6 @@ import TableMante from '@/components/Mante/TableMante';
 import Page_Footer from '@/components/PageFooter';
 import Page_Head from '@/components/PageHead'
 
-import Typewriter from 'typewriter-effect';
-
 export default function indexMante() {
     return (
         <>
