c9f746c70d121946c38cd5e6dadb633074ec80f5
jordanlaguna <126918343+jordanlaguna@users.noreply.github.com>
2023-06-08 20:19:29 -0600

    Update css

--- src/pages/indexInven.tsx
+++ src/pages/indexInven.tsx
@@ -2,7 +2,7 @@ import Page_Footer from "@/components/PageFooter";
 import Page_Head from "@/components/PageHead";
 import TableForm from "@/components/Inventario/FormTable";
 import { FormEvent, useState } from "react";
-
+/*<link rel="stylesheet" href="/styles/Inven.css" />;}*/
 export default function indexInven() {
   return (
     <>
