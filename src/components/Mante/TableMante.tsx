2f5b2dfbe66298adf7e4981b978da6c04a88ceff
JulioJafed <128091133+JulioJafed@users.noreply.github.com>
2023-06-14 00:49:50 -0600

    add popup in TableMante, new css style, removed ModalMante component

--- src/components/Mante/TableMante.tsx
+++ src/components/Mante/TableMante.tsx
@@ -1,21 +1,31 @@
 import PropTypes from 'prop-types'
-import React, { Component, useState } from 'react'
-import ModalMante from '@/components/Mante/ModalMante';
-export default function TableMante() {
-    
-    const[ estadoModal1, cambiarEstadoModal1] = useState(false);      
+import React, { useState } from 'react'
 
+export default function TableMante() {
+    const [isOpen, setIsOpen] = useState(false);
+    const openModal = () => {
+        setIsOpen(true);
+    };
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+    const [isOpen2, setIsOpen2] = useState(false);
+    const openModal2 = () => {
+        setIsOpen2(true);
+    };
+    const closeModal2 = () => {
+        setIsOpen2(false);
+    };
     return (
-         <>
-            
+        <>
 
             <section className='TabMante' >
-            <div className='search-container'>
-                <input className='input-search' type='text' placeholder='Buscar...' />
-                {/*<button className='button-search' type="submit">Buscar</button>*/}
-                <div className='titul2'>Reporte de Buses en Mantenimiento</div>    
-            </div>
-        
+                <div className='search-container'>
+                    <input className='input-search' type='text' placeholder='Buscar...' />
+                    {/*<button className='button-search' type="submit">Buscar</button>*/}
+                    <div className='titul2'>Reporte de Buses en Mantenimiento</div>
+                </div>
+
                 <table className="tabla1" >
                     <thead>
                         <tr>
@@ -35,140 +45,77 @@ export default function TableMante() {
                             <td>Cambio de LLanta</td>
                             <td>$100</td>
                             <td>
-                            <button onClick={()=> cambiarEstadoModal1(!estadoModal1)} className='buntoTabale' about='button' > Mas Información
-                                    <ModalMante
-                                        estado={estadoModal1}
-                                        cambiarEstado={cambiarEstadoModal1}
-                               
-                                    >   
-                                    <section className='contenidoModal'>
-                                            <h1 className='texh1Mo'> Nota</h1>
-                                            <p className='notaModal'> Se le cambio una solo llanta el resto todo bien</p>
-                                            <h1 className='DaModalExit'> Fecha de salida: <h2 className='DaModalExi'>06/06/21 </h2> </h1>
-                                            <select className='optionValue'>
-                                                <option value="Available">Disponible</option>
-                                                <option value="Not available">No disponible</option>
-                                            </select>
-                                            <button className='buttonModal'>Aceptar</button>
-                                        </section>
-                                    </ModalMante>
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>02</td>
-                            <td>648122</td>
-                            <td>06/06/21</td>
-                            <td>Cambio de LLanta</td>
-                            <td>$100</td>
-                            <td>
-                                <button onClick={()=>cambiarEstadoModal1(!estadoModal1)} className='buntoTabale' about='button' > Mas Información
-                                    <ModalMante
-                                        estado = {estadoModal1}
-                                        cambiarEstado= {cambiarEstadoModal1}
-                                       
-                                    >
-                                        <section className='contenidoModal'>
-                                            <h1 className='texh1Mo'> Nota</h1>
-                                            <p className='notaModal'> Se le cambio una solo llanta el resto todo bien</p>
-                                            <h1 className='DaModalExit'> Fecha de salida: <h2 className='DaModalExi'>06/06/21 </h2> </h1>
-                                            <select className='optionValue'>
-                                                <option value="Available">Disponible</option>
-                                                <option value="Not available">No disponible</option>
-                                            </select>
-                                            <button className='buttonModal'>Aceptar</button>
-                                        </section>
-                                    </ModalMante>
-                                </button>
-                            </td>
-                       </tr>
+                                <div>
+                                    <button className='masInfobutton' onClick={openModal}>Más información</button>
+                                    {isOpen && (
 
-                        <tr>
-                            <td>03</td>
-                            <td>648122</td>
-                            <td>06/06/21</td>
-                            <td>Cambio de LLanta</td>
-                            <td>$100</td>
-                            <td>
-                            <button onClick={()=>cambiarEstadoModal1(!estadoModal1)} className='buntoTabale' about='button' > Mas Información
-                                    <ModalMante
-                                        estado = {estadoModal1}
-                                        cambiarEstado= {cambiarEstadoModal1}
-                                       
-                                    >
-                                        <section className='contenidoModal'>
-                                            <h1 className='texh1Mo'> Nota</h1>
-                                            <p className='notaModal'> Se le cambio una solo llanta el resto todo bien</p>
-                                            <h1 className='DaModalExit'> Fecha de salida: <h2 className='DaModalExi'>06/06/21 </h2> </h1>
-                                            <select className='optionValue'>
-                                                <option value="Available">Disponible</option>
-                                                <option value="Not available">No disponible</option>
-                                            </select>
-                                            <button className='buttonModal'>Aceptar</button>
-                                        </section>
-                                    </ModalMante>
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>04</td>
-                            <td>648122</td>
-                            <td>06/06/21</td>
-                            <td>Cambio de LLanta</td>
-                            <td>$100</td>
-                            <td> 
-                                <button onClick={()=>cambiarEstadoModal1(!estadoModal1)} className='buntoTabale' about='button' > Mas Información
-                                    <ModalMante
-                                        estado={estadoModal1}
-                                        cambiarEstado={cambiarEstadoModal1}
-                                        
-                                    >
-                                        <section className='contenidoModal'>
-                                            <h1 className='texh1Mo'> Nota</h1>
-                                            <p className='notaModal'> Se le cambio una solo llanta el resto todo bien</p>
-                                            <h1 className='DaModalExit'> Fecha de salida: <h2 className='DaModalExi'>06/06/21 </h2> </h1>
-                                            <select className='optionValue'>
-                                                <option value="Available">Disponible</option>
-                                                <option value="Not available">No disponible</option>
-                                            </select>
-                                            <button className='buttonModal'>Aceptar</button>
-                                        </section>
-                                    </ModalMante>
-                                </button>
-                            </td>
-                        </tr>
+                                        <div className="modal-overlay">
 
-                        <tr>
-                            <td>05</td>
-                            <td>648122</td>
-                            <td>06/06/21</td>
-                            <td>Cambio de LLanta</td>
-                            <td>$100</td>
-                            <td>
-                            <button onClick={()=> cambiarEstadoModal1(!estadoModal1)} className='buntoTabale' about='button' > Mas Información
-                                    <ModalMante
-                                        estado = {estadoModal1}
-                                        cambiarEstado= {cambiarEstadoModal1}
-                                        /*tituloModal=  "INFORMACIÓN"*/
-                                    >
-                                        <section className='contenidoModal'>
-                                            <h1 className='texh1Mo'> Nota</h1>
-                                            <p className='notaModal'> Se le cambio una solo llanta el resto todo bien</p>
-                                            <h1 className='DaModalExit'> Fecha de salida: <h2 className='DaModalExi'>06/06/21 </h2> </h1>
-                                            <select className='optionValue'>
-                                                <option value="Available">Disponible</option>
-                                                <option value="Not available">No disponible</option>
-                                            </select>
-                                            <button className='buttonModal'>Aceptar</button>
-                                        </section>
-                                    </ModalMante>
-                                </button>
+                                            <div className="modal-content1">
+                                                <button className='.modal-content button buttonMas'  onClick={closeModal}>X</button>
+
+
+                                                <h1 className='texh1Mo'> Más Información</h1>
+                                                
+                                                <p className='notaModal1'> Reporte General</p>
+                                                <p className='notaModal'> Se le cambio una solo llanta el resto todo bien</p>
+
+                                                <h1 className='DaModalExit'> Fecha de salida: <h2 className='notaModal'>06/06/21 </h2> </h1>
+
+                                                <h1 className='DaModalExit'> Estado: <h2 className='notaModal'>Disponible</h2> </h1>
+                                                <h1 className='DaModalExit'> Tipo de Servicio: <h2 className='notaModal'>Preventivo</h2> </h1>
+                                                 
+
+                                                <button className='buttonAcep' onClick={closeModal}>Aceptar</button>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
                             </td>
-                         </tr>
+                        </tr>
 
                     </tbody>
                 </table>
             </section>
+            <div>
+                <button className="buttonSearch" onClick={openModal2} >Agregar</button>
+                {isOpen2 && (
+                    <div className="modal-overlay2">
+                        <div className="modal-content">
+                            <button className='.modal-content button ' onClick={closeModal2}>X</button>
+
+                            <h1 className='texh2Add'>  ID:</h1>
+                            <input className='impuh1Add' type="texts" placeholder=' ID' />
+
+                            <h1 className='texh2Add'>  Numero De Placa:</h1>
+                            <input className='impuh1Add' type="texts" placeholder=' N° Placa' />
+
+                            <h2 className='texh2Add'> Fecha de Entrada</h2>
+                            <input className='impuh1Add' type="date" />
+
+                            <h2 className='texh2Add'> Reporte/Tipo de Servicio</h2>
+                            <input className='impuh1Add' type="text" placeholder=' Nota' />
+                            <select className='optionValueAdd2'>
+                                <option value="Not available" placeholder='seccioner'>...</option>
+                                <option value="Available">Preventivo </option>
+                                <option value="Not available">General</option>
+                            </select>
+
+                            <h2 className='texh2Add'> Costo</h2>
+                            <input className='impuh1Add' type="number" placeholder=' $0.0' />
+
+                            <h2 className='texh2Add'> Estado:</h2>
+                            <select className='optionValueAdd2'>
+                                <option value="Not available" placeholder='seccioner'>...</option>
+                                <option value="Available">Disponible</option>
+                                <option value="Not available">No disponible</option>
+                            </select>
+
+                            <input className='buttonAcep' type="submit" onClick={closeModal2} />
+                        </div>
+                    </div>
+                )}
+            </div>
         </>
     )
 }
